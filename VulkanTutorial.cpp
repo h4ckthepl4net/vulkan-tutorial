68bbc81fb0ca9db0ec9f711cb45f1d659aaa2156
Karapet Sahatchian <k.sahatchian@codeex.io>
2025-01-26 00:18:12 +0400

    Add vulkan to dependencies

--- VulkanTutorial.cpp
+++ VulkanTutorial.cpp
@@ -1,6 +1,7 @@
 // VulkanTutorial.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <Windows.h>
+#include <vulkan/vulkan.h>
 #include <iostream>
 
 int main()
